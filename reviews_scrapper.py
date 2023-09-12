acbd56e9eaecbabf8f9408a6681ef22dbdbbb68a
barb <bsanser@gmail.com>
2023-09-12 16:25:47 +0100

    feat: modify some strings

--- reviews_scrapper.py
+++ reviews_scrapper.py
@@ -10,11 +10,11 @@ class ReviewItem:
   size: str
   date: str
   country: str
-  rating: str
+  rating: float
   title: str
   body: str
   verified_purchase: str 
-  people_who_found_helpful: str
+  people_who_found_helpful: int
   author: str
 
 
@@ -26,17 +26,22 @@ def get_html(page, asin):
   return html
 
 def parse_html(html, asin):
+  date_country_string = html.css_first("#customer_review-R3UWUBUOZUDI2B > span").text(strip=True)
+  country_split_word = "the"
+  date_split_word = "on"
+  country = date_country_string.split(country_split_word,1)[1].split(date_split_word, )[0]
+  date = date_country_string.split(date_split_word,1)[1]
   review_item = ReviewItem(
     asin = asin,
     product_title = html.css_first("span#productTitle").text(strip=True),
-    size = html.css_first("#customer_review-R3UWUBUOZUDI2B > div.a-row.a-spacing-mini.review-data.review-format-strip > span").text(strip=True),
-    date = html.css_first("#customer_review-R3UWUBUOZUDI2B > span").text(strip=True),
-    country = html.css_first("#customer_review-R3UWUBUOZUDI2B > span").text(strip=True), 
-    rating = html.css_first("#customer_review-R3UWUBUOZUDI2B > div:nth-child(2) > a > i").text(strip=True),
+    size = html.css_first("#customer_review-R3UWUBUOZUDI2B > div.a-row.a-spacing-mini.review-data.review-format-strip > span").text(strip=True)[6:],
+    date = date.strip(),
+    country = country.strip(),
+    rating = float(html.css_first("#customer_review-R3UWUBUOZUDI2B > div:nth-child(2) > a > i").text(strip=True)[:3]),
     title = html.css_first("#customer_review-R3UWUBUOZUDI2B > div:nth-child(2) > a > span:nth-child(3)").text(strip=True),
     body = html.css_first("#customer_review-R3UWUBUOZUDI2B > div.a-row.a-spacing-small.review-data > span > div > div.a-expander-content.reviewText.review-text-content.a-expander-partial-collapse-content > span").text(strip=True),
     verified_purchase = html.css_first("#customer_review-R3UWUBUOZUDI2B > div.a-row.a-spacing-mini.review-data.review-format-strip > a > span").text(strip=True),
-    people_who_found_helpful = html.css_first("#customer_review-R3UWUBUOZUDI2B > div.a-row.review-comments.cr-vote-action-bar > span.cr-vote > div.a-row.a-spacing-small > span").text(strip=True),
+    people_who_found_helpful = int(html.css_first("#customer_review-R3UWUBUOZUDI2B > div.a-row.review-comments.cr-vote-action-bar > span.cr-vote > div.a-row.a-spacing-small > span").text(strip=True).split(" ", 1)[0]),
     author = html.css_first("#customer_review-R3UWUBUOZUDI2B > div:nth-child(1) > a > div.a-profile-content > span").text(strip=True)
   )
   return review_item
