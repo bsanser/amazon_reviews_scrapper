f848bfb3135809ff5e062b73396fc020c4036f6a
barb <bsanser@gmail.com>
2023-09-18 19:08:53 +0100

    feat: saved global ratings to excel

--- scrapper.py
+++ scrapper.py
@@ -29,33 +29,32 @@ def get_reviews(html):
     except:
         pass
 
-def save(results):
+def save(results, name):
   df = pd.DataFrame(results)
-  df.to_excel(f'{asin}-reviews.xlsx', index = False)
+  df.to_excel(f'{asin}-{name}.xlsx', index = False)
 
 def run():
   pw = sync_playwright().start()
   browser = pw.chromium.launch()
   page = browser.new_page()
   global_html = get_html(page,f'https://www.amazon.com/product-reviews/{asin}?th=1')
-  average_star_rating = float(global_html.find("i", attrs={'data-hook': 'average-star-rating'}).string.replace(' out of 5 stars', '').strip())
-  total_review_count =  int(global_html.find_all(class_='averageStarRatingNumerical')[0].string.replace(' global ratings', '').replace(',', '').strip())
-  global_ratings = {}
+  global_rating = {
+    "average_star_rating" : [float(global_html.find("i", attrs={'data-hook': 'average-star-rating'}).string.replace(' out of 5 stars', '').strip())],
+    "total_review_count" : [int(global_html.find_all(class_='averageStarRatingNumerical')[0].string.replace(' global ratings', '').replace(',', '').strip())],
+  }
+  
   rows = global_html.find_all('tr', class_="a-histogram-row")
   for row in rows:
     cells = row.find_all("td")
     rating = cells[0].find('a', class_="a-link-normal").text.strip()
     percentage = cells[2].find('a', class_="a-link-normal").text.strip()
-    global_ratings[rating]=percentage
+    global_rating[rating]=[percentage]
   
-  print(f'Average rating: {average_star_rating}')
-  print(f'Total number of reviews: {total_review_count}')
-  print(f'Rating distribution: {global_ratings}')
+  save(global_rating,"global-rating")
 
   for x in range(2000):
     soup = get_html(page,f'https://www.amazon.com/product-reviews/{asin}/ref=cm_cr_arp_d_paging_btm_next_2?ie=UTF8&reviewerType=all_reviews&sortBy=recent&pageNumber={x+1}')
     get_reviews(soup)
-    # save(reviews_list)
     if not soup.find('li', {'class': 'a-disabled a-last'}):
         pass
     else:
@@ -64,7 +63,7 @@ def run():
   browser.close()
   pw.stop()
   
-  save(reviews_list)
+  save(reviews_list,"reviews-list")
   print('Fin')
 
 def main():
