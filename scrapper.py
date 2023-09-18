6e9a75c2a682a22d004f60df2afdc84b3f076baa
barb <bsanser@gmail.com>
2023-09-18 19:15:59 +0100

    feat: encapsulated logic for getting global ratings

--- scrapper.py
+++ scrapper.py
@@ -29,6 +29,25 @@ def get_reviews(html):
     except:
         pass
 
+def get_global_ratings(page):
+  try:
+    global_html = get_html(page,f'https://www.amazon.com/product-reviews/{asin}?th=1')
+    global_rating = {
+      "average_star_rating" : [float(global_html.find("i", attrs={'data-hook': 'average-star-rating'}).string.replace(' out of 5 stars', '').strip())],
+      "total_review_count" : [int(global_html.find_all(class_='averageStarRatingNumerical')[0].string.replace(' global ratings', '').replace(',', '').strip())],
+    }
+    
+    rows = global_html.find_all('tr', class_="a-histogram-row")
+    for row in rows:
+      cells = row.find_all("td")
+      rating = cells[0].find('a', class_="a-link-normal").text.strip()
+      percentage = cells[2].find('a', class_="a-link-normal").text.strip()
+      global_rating[rating]=[percentage]
+    
+    return global_rating
+  except:
+        pass
+  
 def save(results, name):
   df = pd.DataFrame(results)
   df.to_excel(f'{asin}-{name}.xlsx', index = False)
@@ -37,29 +56,14 @@ def run():
   pw = sync_playwright().start()
   browser = pw.chromium.launch()
   page = browser.new_page()
-  global_html = get_html(page,f'https://www.amazon.com/product-reviews/{asin}?th=1')
-  global_rating = {
-    "average_star_rating" : [float(global_html.find("i", attrs={'data-hook': 'average-star-rating'}).string.replace(' out of 5 stars', '').strip())],
-    "total_review_count" : [int(global_html.find_all(class_='averageStarRatingNumerical')[0].string.replace(' global ratings', '').replace(',', '').strip())],
-  }
-  
-  rows = global_html.find_all('tr', class_="a-histogram-row")
-  for row in rows:
-    cells = row.find_all("td")
-    rating = cells[0].find('a', class_="a-link-normal").text.strip()
-    percentage = cells[2].find('a', class_="a-link-normal").text.strip()
-    global_rating[rating]=[percentage]
-  
-  save(global_rating,"global-rating")
-
+  save(get_global_ratings(page),"global-rating")
   for x in range(2000):
     soup = get_html(page,f'https://www.amazon.com/product-reviews/{asin}/ref=cm_cr_arp_d_paging_btm_next_2?ie=UTF8&reviewerType=all_reviews&sortBy=recent&pageNumber={x+1}')
     get_reviews(soup)
     if not soup.find('li', {'class': 'a-disabled a-last'}):
         pass
     else:
-        break
-  print(f"this is the len of reviews {len(reviews_list)}")  
+        break 
   browser.close()
   pw.stop()
   
